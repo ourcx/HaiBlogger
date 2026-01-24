570bbcc1a5eb9e7148dc40cbc758e63f3e1a57ae
小海 <3277975910@qq.com>
2026-01-24 12:14:14 +0800

    fix: 白屏

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import VueDevTools from 'vite-plugin-vue-devtools';
 export default defineConfig({
   // **重要**：添加 'base' 选项以指定你的 GitHub 仓库名称。
   // 请将 '/your-repo-name/' 替换为你的实际仓库名，例如 '/xh-blog/'.
-  // base: '/HaiBlogger/', 
+  base: '/HaiBlogger/', 
   plugins: [vue(),VueDevTools()],
   resolve: {
     alias: {
